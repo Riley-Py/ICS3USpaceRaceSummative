ad5e98b962529502adbf85b8f68211a88a3d3790
Riley <riley.c.gamer@gmail.com>
2022-05-14 21:16:00 -0400

    Lots of features added

--- ICS3USpaceRaceSummative/Form1.Designer.cs
+++ ICS3USpaceRaceSummative/Form1.Designer.cs
@@ -31,11 +31,15 @@
             this.components = new System.ComponentModel.Container();
             this.gameTimer = new System.Windows.Forms.Timer(this.components);
             this.label1 = new System.Windows.Forms.Label();
+            this.scoreLabel1 = new System.Windows.Forms.Label();
+            this.scoreLabel2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // gameTimer
             // 
+            this.gameTimer.Enabled = true;
             this.gameTimer.Interval = 45;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
             // 
             // label1
             // 
@@ -46,12 +50,36 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
             // 
+            // scoreLabel1
+            // 
+            this.scoreLabel1.AutoSize = true;
+            this.scoreLabel1.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel1.ForeColor = System.Drawing.Color.White;
+            this.scoreLabel1.Location = new System.Drawing.Point(9, 489);
+            this.scoreLabel1.Name = "scoreLabel1";
+            this.scoreLabel1.Size = new System.Drawing.Size(142, 24);
+            this.scoreLabel1.TabIndex = 1;
+            this.scoreLabel1.Text = "scoreLabel1";
+            // 
+            // scoreLabel2
+            // 
+            this.scoreLabel2.AutoSize = true;
+            this.scoreLabel2.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel2.ForeColor = System.Drawing.Color.White;
+            this.scoreLabel2.Location = new System.Drawing.Point(646, 489);
+            this.scoreLabel2.Name = "scoreLabel2";
+            this.scoreLabel2.Size = new System.Drawing.Size(142, 24);
+            this.scoreLabel2.TabIndex = 2;
+            this.scoreLabel2.Text = "scoreLabel2";
+            // 
             // SpaceRace
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 550);
+            this.Controls.Add(this.scoreLabel2);
+            this.Controls.Add(this.scoreLabel1);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "SpaceRace";
@@ -68,6 +96,8 @@
 
         private System.Windows.Forms.Timer gameTimer;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label scoreLabel1;
+        private System.Windows.Forms.Label scoreLabel2;
     }
 }
 
--- ICS3USpaceRaceSummative/Form1.cs
+++ ICS3USpaceRaceSummative/Form1.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-
+/*********************************************************************************
+ * Author: Riley Cant
+ * Course: ICS3U
+ * Program: Space Race
+ * Description: A clone of the Atari game "Space Race" from the Summer of 1973
+ * Date of comment: 05/14/22
+ ********************************************************************************/
 namespace ICS3USpaceRaceSummative
 {
     public partial class SpaceRace : Form
@@ -21,6 +23,25 @@ namespace ICS3USpaceRaceSummative
         bool sDown = false;
         bool upKey = false;
         bool downKey = false;
+
+        Rectangle player1 = new Rectangle(230, 450, 10, 30);
+        Rectangle player2 = new Rectangle(550, 450, 10, 30);
+
+        List<Rectangle> leftSide = new List<Rectangle>();
+        List<Rectangle> rightSide = new List<Rectangle>();
+
+        List<int> rectangleSpeedLeft = new List<int>();
+        List<int> rectangleSpeedRight = new List<int>();
+
+        int playerSpeed = 5;
+        int misslesX = 5;
+        int misslesY = 7;
+        int player1Score = 0;
+        int player2Score = 0;
+
+        Random randGen = new Random();
+
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
 
         private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
@@ -66,6 +87,148 @@ namespace ICS3USpaceRaceSummative
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
+            e.Graphics.FillRectangle(whiteBrush, player1);
+            e.Graphics.FillRectangle(whiteBrush, player2);
+
+            for (int i = 0; i < leftSide.Count(); i++)
+            {
+                e.Graphics.FillRectangle(whiteBrush, leftSide[i]);
+            }
+            for (int i = 0; i < rightSide.Count(); i++)
+            {
+                e.Graphics.FillRectangle(whiteBrush, rightSide[i]);
+            }
+
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            playerMovements();
+
+            leftSideMovements();
+
+            rightSideMovements();
+
+            collision();
+
+            Refresh();
+
+        }
+
+        public void playerMovements()
+        {
+            if (wDown == true && player1.Y > 0)
+            {
+                player1.Y -= playerSpeed;
+
+            }
+            if (upKey == true && player2.Y > 0)
+            {
+                player2.Y -= playerSpeed;
+            }
+            if (sDown == true && player1.Y < this.Height - player1.Height)
+            {
+                player1.Y += playerSpeed;
+            }
+            if (downKey == true && player2.Y < this.Height - player2.Height)
+            {
+                player2.Y += playerSpeed;
+            }
+
+        }
+
+        public void leftSideMovements()
+        {
+            for (int i = 0; i < leftSide.Count(); i++)
+            {
+                int x = leftSide[i].X + rectangleSpeedLeft[i];
+                leftSide[i] = new Rectangle(x, leftSide[i].Y, misslesX, misslesY);
+
+                if (leftSide[i].X >= this.Width)
+                {
+                    leftSide.RemoveAt(i);
+                    rectangleSpeedLeft.RemoveAt(i);
+                }
+
+            }
+            int y = randGen.Next(10, 400);
+            if (leftSide.Count() < 15)
+            {
+                leftSide.Add(new Rectangle(10, y, misslesX, misslesY));
+                rectangleSpeedLeft.Add(randGen.Next(5, 10));
+
+            }
+
+        }
+        public void rightSideMovements()
+        {
+            for (int i = 0; i < rightSide.Count(); i++)
+            {
+                int x = rightSide[i].X - rectangleSpeedRight[i];
+                rightSide[i] = new Rectangle(x, rightSide[i].Y, misslesX, misslesY);
+
+                if (rightSide[i].X <= 0)
+                {
+                    rightSide.RemoveAt(i);
+                    rectangleSpeedRight.RemoveAt(i);
+                }
+
+            }
+            int y = randGen.Next(10, 400);
+            if (rightSide.Count() < 10)
+            {
+                rightSide.Add(new Rectangle(790, y, misslesX, misslesY));
+                rectangleSpeedRight.Add(randGen.Next(5, 10));
+            }
+
+        }
+
+        public void collision()
+        {
+            for (int i = 0; i < leftSide.Count(); i++)
+            {
+                for (int i2 = 0; i2 < rightSide.Count; i2++)
+                {
+                    if (player1.IntersectsWith(leftSide[i]) || player1.IntersectsWith(rightSide[i2]))
+                    {
+                        restartPlayer1();
+                    }
+                    else if (player2.IntersectsWith(leftSide[i]) || player2.IntersectsWith(rightSide[i2]))
+                    {
+                        restartPlayer2();
+                    }
+                }
+            }
+            if (player1.Y == 0)
+            {
+                player1Score++;
+                restartPlayer1();
+                scoring();
+                
+
+            }
+            if (player2.Y == 0)
+            {
+                player2Score++;
+                restartPlayer2();
+                scoring();
+            }
+        }
+        public void restartPlayer1()
+        {
+            player1.X = 230;
+            player1.Y = 450;
+        }
+        public void restartPlayer2()
+        {
+            player2.X = 550;
+            player2.Y = 450;
+        }
+
+        public void scoring()
+        {
+            scoreLabel1.Text = $"{player1Score}";
+            scoreLabel2.Text = $"{player2Score}";
 
         }
     }
